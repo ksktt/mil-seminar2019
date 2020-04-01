242d34d7c745153a8ff4ef0976f80f1a7aa7ab66
kusakatakuya <piyo@fuga>
2020-04-01 21:03:54 +0900

    Modify how to import dataset and warmup schduler

--- mil-seminar2019/src/hyperparam_optuna.py
+++ mil-seminar2019/src/hyperparam_optuna.py
@@ -10,27 +10,33 @@ import optuna
 from data import CifarDataset
 
 def train(args, model, device, train_loader, optimizer, epoch):
-        model.train()
-        for batch_idx, (fname, data, target) in enumerate(train_loader):
-            data, target = data.to(device), target.to(device)
-            optimizer.zero_grad()
-            output = model(data)
-            loss = F.nll_loss(output, target)
-            loss.backward()
-            optimizer.step()
-            if batch_idx % args.log_interval == 0:
-                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                    epoch, batch_idx * len(data), len(train_loader.dataset),
-                    100. * batch_idx / len(train_loader), loss.item()))
+    train_iter_num = round(len(train_dataset)/ batch_size)
+    model.train()
+    train_data_iter = iter(train_loader)
+    for i in range(train_iter_num):
+        optimizer.zero_grad()
+        fnames, labels, imgs = train_data_iter.next()
+        train_data, target = imgs.to(device), labels.to(device)
+        output = model(train_data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
 
 def test(args, model, device, test_loader):
         model.eval()
         test_loss = 0
         correct = 0
+        test_iter_num = round(len(test_dataset)/ batch_size)
+        test_data_iter = iter(test_loader)
         with torch.no_grad():
-            for fname, data, target in test_loader:
-                data, target = data.to(device), target.to(device)
-                output = model(data)
+            for i in range (test_iter_num):
+                fnames, labels, imgs = test_data_iter.next()
+                val_data, target = imgs.to(device), labels.to(device)
+                output = model(val_data)
                 # sum up batch loss
                 test_loss += F.nll_loss(output, target, reduction='sum').item()
                 # get the index of the max log-probability
@@ -44,7 +50,6 @@ def test(args, model, device, test_loader):
             test_loss, correct, len(test_loader.dataset),
             100. * correct / len(test_loader.dataset)))
 
-"""
 def wrap_scheduler(initial_lr, first_time, second_time, third_time, epoch):
     first_epoch = int(epochs_num*first_time)
     second_epoch = int(epochs_num*second_time)
@@ -57,29 +62,35 @@ def wrap_scheduler(initial_lr, first_time, second_time, third_time, epoch):
         return initial_lr / 5
     elif epoch / epochs_num >third_time:
         return initial_lr / (5*5)
-"""
 
-def wrap_scheduler(initial_lr, first_time, second_time):
+def wrap_scheduler(initial_lr, first_time, second_time, third_time):
     accuracy = 0
     max_accuracy = 0
     first_epoch = int(epochs_num*first_time)
     second_epoch = int(epochs_num*second_time)
-    #third_epoch = int(epochs_num*third_time)
+    third_epoch = int(epochs_num*third_time)
+    
+    train_iter_num = round(len(train_dataset)/ batch_size)
+    val_iter_num = round(len(val_dataset)/ batch_size)
+
     for epoch in range(1, epochs_num+1):
-        if epoch <= first_epoch:
-            return 10**(-2.0+0.4*epoch)*initial_lr
-        x = initial_lr * 5
-        if epoch >= second_epoch:
-            x /= 5
-        #if epoch >= third_epoch:
-        #    x /= 5
+        if epoch / epochs_num <= first_time:
+            x =  (10**(-2.0+2.0*(epoch / first_epoch)))*initial_lr
+        elif epoch / epochs_num <= second_time:
+            x =  initial_lr
+        elif epoch / epochs_num <= third_time:
+            x =  initial_lr / 5
+        elif epoch / epochs_num > third_time:
+            x =  initial_lr / (5*5)
 
         optimizer = optim.Adadelta(model.parameters(), lr=x)
 
         model.train()
-        for batch_idx, (train_data, target) in enumerate(train_loader):
-            train_data, target = train_data.to(device), target.to(device)
+        train_data_iter = iter(train_loader)
+        for i in range(train_iter_num):
             optimizer.zero_grad()
+            fnames, labels, imgs = train_data_iter.next()
+            train_data, target = imgs.to(device), labels.to(device)
             output = model(train_data)
             loss = F.nll_loss(output, target)
             loss.backward()
@@ -88,9 +99,11 @@ def wrap_scheduler(initial_lr, first_time, second_time):
         model.eval()
         test_loss = 0
         correct = 0
+        val_data_iter = iter(val_loader)
         with torch.no_grad():
-            for val_data, target in val_loader:
-                val_data, target = val_data.to(device), target.to(device)
+            for j in range (val_iter_num):
+                fnames, labels, imgs = val_data_iter.next()
+                val_data, target = imgs.to(device), labels.to(device)
                 output = model(val_data)
                 # sum up batch loss
                 test_loss += F.nll_loss(output, target, reduction='sum').item()
@@ -110,16 +123,9 @@ def wrap_scheduler(initial_lr, first_time, second_time):
 def objective(trial):
     initial_lr = trial.suggest_uniform('initial_lr', 0.001, 0.1)
     first_time = trial.suggest_uniform('first_time', 0.2, 0.5)
-    second_time = trial.suggest_uniform('second_time', 0.7, 0.9)
-    return wrap_scheduler(initial_lr, first_time, second_time)
-
-def wrap_lr(initial_lr, first_time, second_time,epoch):
-    if epoch <= first_time:
-        return 10**(-2.0+0.4*epoch)*initial_lr
-    x = initial_lr * 5
-    if epoch >= second_time:
-        x /= 5
-    return x
+    second_time = trial.suggest_uniform('second_time', 0.6, 0.7)
+    third_time = trial.suggest_uniform('third_time', 0.8, 0.9)
+    return wrap_scheduler(initial_lr, first_time, second_time, third_time)
 
 class Hyperparams:
     ''' Hyperparameter class
@@ -136,6 +142,7 @@ class Hyperparams:
         self.hyperparam0 = 0.01 #init_lr
         self.hyperparam1 = 0.02 #first_time
         self.hyperparam2 = 0.03 #second_time
+        self.htperparam3 = 0.04 #third_time
 
 def tune_hyperparams(args: Namespace, task: Task, preprocess_func: Compose, model: Module) -> Hyperparams:
     ''' Tune hyperparameters
@@ -152,8 +159,9 @@ def tune_hyperparams(args: Namespace, task: Task, preprocess_func: Compose, mode
     Hyperparams.hyperparam0 = study.best_params['initial_lr']
     Hyperparams.hyperparam1 = study.best_params['first_time']
     Hyperparams.hyperparam2 = study.best_params['second_time']
+    Hyperparams.hyperparam3 = study.best_params['third_time']
     
-    param_list = [study.best_params['initial_lr'], study.best_params['first_time'], study.best_params['second_time']]
+    param_list = [study.best_params['initial_lr'], study.best_params['first_time'], study.best_params['second_time'], study.best_params['third_time']]
 
     return param_list
 
@@ -233,6 +241,8 @@ if __name__ == '__main__':
 
     epochs_num = args.epochs
 
+    batch_size = args.batch_size
+
     ############################################################
     # Instantiate task object
     task = Task(args.task)
@@ -258,7 +268,6 @@ if __name__ == '__main__':
     hyperparam = tune_hyperparams(args, task, preprocess_func, model)
     ############################################################
 
-    warmup_lr = wrap_lr(hyperparam.hyperparam0, hyperparam.hyperparam1, hyperparam.hyperparam2, epoch)
-    optimizer = optim.Adadelta(model.parameters(), lr=warmup_lr)
-    train(args, model, device, train_loader, optimizer, epoch)
-    test(args, model, device, test_loader)
+    #optimizer = optim.Adadelta(model.parameters(), lr=warmup_lr)
+    #train(args, model, device, train_loader, optimizer, epoch)
+    #test(args, model, device, test_loader)
